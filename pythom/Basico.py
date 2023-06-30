5a0ce25affc66fcc10576c1833f297f94f3776ca
Noobpro112 <125266353+Noobpro112@users.noreply.github.com>
2023-06-30 12:32:11 -0300

    Add files via upload

---
+++ pythom/Basico.py
@@ -0,0 +1,12 @@
+print("Ola Mundo")
+
+numero = int(input("Digite o seu numero: "))
+
+print ("Voce digitou o numero:")
+print (numero)
+
+print(f'você digitou o numero: {numero}')
+
+
+decimal = float
+inteiros = int
