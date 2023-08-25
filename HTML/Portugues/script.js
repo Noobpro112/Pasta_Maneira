2bc482cda3b16367bce5a2cbec5f1c14aa495088
Noobpro112 <reismatheusyan83@gmail.com>
2023-08-25 12:25:39 -0300

    Começo do script e modificações no html

---
+++ HTML/Portugues/script.js
@@ -0,0 +1,13 @@
+function facil(){
+    document.getElementById('facil').style.display = 'none';
+}
+function intermediario(){
+    document.getElementById('facil').style.display = 'none';
+    document.getElementById('facil').innerHTML;
+}
+function dificil(){
+    document.getElementById('facil').style.display = 'none';
+}
+function adaptativo(){
+    document.getElementById('facil').style.display = 'none';
+}
