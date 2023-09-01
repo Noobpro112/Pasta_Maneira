5ee82f8ea00c4c7f3529391d98322cb99f61cdcb
Noobpro112 <reismatheusyan83@gmail.com>
2023-09-01 09:07:15 -0300

    EU RESOLVI O PROBLEMA DO FLUTTER, TOMA ARQUIVOS NORMAIS!!!!

---
+++ HTML/Portugues/script.js
@@ -0,0 +1,13 @@
+function facil(){
+    document.getElementById('facil').style.display = 'none';
+}
+function intermediario(){
+    document.getElementById('facil').style.display = 'none';
+    document.getElementById('facil').innerHTML = "a";
+}
+function dificil(){
+    document.getElementById('facil').style.display = 'none';
+}
+function adaptativo(){
+    document.getElementById('facil').style.display = 'none';
+}
