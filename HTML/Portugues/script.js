5abaa045ad0a3bb640ea905499c011b7b9f481eb
Noobpro112 <reismatheusyan83@gmail.com>
2023-09-01 11:05:58 -0300

    a sofrencia começou :( (java script dos sites.)

--- HTML/Portugues/script.js
+++ HTML/Portugues/script.js
@@ -11,3 +11,42 @@ function dificil(){
 function adaptativo(){
     document.getElementById('facil').style.display = 'none';
 }
+
+function correto1(){
+    let caixa_Misteriosa = document.getElementById('respostas1');
+    caixa_Misteriosa.style.display==='none' ? caixa_Misteriosa.style.display = 'block' : caixa_Misteriosa.style.display = 'none'
+    document.getElementById('User').innerHTML = 'Você acertou!'
+}
+function errado1(){
+    let caixa_misteriosa = document.getElementById('respostas1');
+    caixa_misteriosa.style.display==='none' ? caixa_misteriosa.style.display = 'block' : caixa_misteriosa.style.display = 'none'
+    document.getElementById('User').innerHTML = 'Você Errou!'
+    
+    document.getElementById('justi1').innerHTML = 'A oração coordenada adversativa é introduzida pela conjunção "mas", indicando oposição ou contraste entre "Ele estudou muito" e "não conseguiu passar no exame".'
+}
+
+function correto2(){
+    let caixa_Misteriosa = document.getElementById('respostas2');
+    caixa_Misteriosa.style.display==='none' ? caixa_Misteriosa.style.display = 'block' : caixa_Misteriosa.style.display = 'none'
+    document.getElementById('User2').innerHTML = 'Você acertou!'
+}
+function errado2(){
+    let caixa_misteriosa = document.getElementById('respostas2');
+    caixa_misteriosa.style.display==='none' ? caixa_misteriosa.style.display = 'block' : caixa_misteriosa.style.display = 'none'
+    document.getElementById('User2').innerHTML = 'Você Errou!'
+    
+    document.getElementById('justi2').innerHTML = 'As alternativas "nadar" e "correr" estão ligadas por uma conjunção "e", indicando adição, ou seja, a ideia é que ela gosta de fazer ambas as atividades.'
+}
+
+function correto3(){
+    let caixa_Misteriosa = document.getElementById('respostas3');
+    caixa_Misteriosa.style.display==='none' ? caixa_Misteriosa.style.display = 'block' : caixa_Misteriosa.style.display = 'none'
+    document.getElementById('User3').innerHTML = 'Você acertou!'
+}
+function errado3(){
+    let caixa_misteriosa = document.getElementById('respostas3');
+    caixa_misteriosa.style.display==='none' ? caixa_misteriosa.style.display = 'block' : caixa_misteriosa.style.display = 'none'
+    document.getElementById('User3').innerHTML = 'Você Errou!'
+    
+    document.getElementById('justi3').innerHTML = 'A oração coordenada aditiva é introduzida pela conjunção "e", indicando a adição das atividades "toma café" e "lê o jornal".'
+}
