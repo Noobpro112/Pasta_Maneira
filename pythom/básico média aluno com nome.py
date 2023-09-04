28e4a09286c61a3321a32e02cdc5b6667d1af70e
Matheus Yan dos Reis <reismatheusyan83@gmail.com>
2023-09-04 11:56:07 -0300

    python

--- pythom/básico média aluno com nome.py
+++
@@ -1,5 +0,0 @@
-nota1 = float(input("Qual sua primeira nota?: "))
-nota2 = float(input("Qual sua segunda nota?: "))
-nome = input("qual seu nome?: ")
-media = (nota1+nota2)/2
-print(f"o Aluno com o nome: {nome} obeteve a média: {media}")
